316207a0b1f03fbed26239516674a06f69acdca8
Mauro Oviedo <mauro.a.oviedo@gmail.com>
2021-12-19 15:49:28 -0500

    test(contact): add new tests for content policy

--- test/phone_book_web/controllers/contact_controller_test.exs
+++ test/phone_book_web/controllers/contact_controller_test.exs
@@ -1,7 +1,7 @@
 defmodule PhoneBookWeb.ContactControllerTest do
   use PhoneBookWeb.ConnCase
 
-  import PhoneBook.ContactsFixtures
+  import PhoneBook.{AccountsFixtures, ContactsFixtures}
   alias PhoneBook.Contacts
   alias PhoneBook.Contacts.Contact
 
@@ -15,7 +15,15 @@ defmodule PhoneBookWeb.ContactControllerTest do
 
   setup %{conn: conn} do
     conn = put_req_header(conn, "accept", "application/json")
-    register_and_log_in_user(%{conn: conn})
+    %{conn: conn, user: user} = register_and_log_in_user(%{conn: conn})
+
+    {:ok, contact} =
+      PhoneBook.Contacts.create_contact(%{
+        name: "some name",
+        user_id: user.id
+      })
+
+    %{conn: conn, user: user, contact: contact}
   end
 
   describe "create contact" do
@@ -39,20 +47,24 @@ defmodule PhoneBookWeb.ContactControllerTest do
   end
 
   describe "index" do
-    test "lists all contacts", %{conn: conn} do
+    test "lists all contacts", %{conn: conn, contact: contact} do
       conn = get(conn, Routes.contact_path(conn, :index))
-      assert json_response(conn, 200)["data"] == []
+
+      assert json_response(conn, 200)["data"] == [
+               %{
+                 "id" => contact.id,
+                 "name" => contact.name,
+                 "phones" => []
+               }
+             ]
     end
 
-    test "lists all contacts with associated phone number(s)", %{conn: conn} do
-      conn = post(conn, Routes.contact_path(conn, :create), contact: @create_attrs)
-      contact = json_response(conn, 201)["data"]
-
+    test "lists all contacts with associated phone number(s)", %{conn: conn, contact: contact} do
       {:ok, phone} =
         Contacts.create_phone(%{
           number: "+17182347788",
           label: :mobile,
-          contact_id: contact["id"]
+          contact_id: contact.id
         })
 
       phone = %{
@@ -63,7 +75,17 @@ defmodule PhoneBookWeb.ContactControllerTest do
       }
 
       conn = get(conn, Routes.contact_path(conn, :index))
-      assert json_response(conn, 200)["data"] == [Map.put(contact, "phones", [phone])]
+
+      assert json_response(conn, 200)["data"] == [
+               Map.put(
+                 %{
+                   "name" => contact.name,
+                   "id" => contact.id
+                 },
+                 "phones",
+                 [phone]
+               )
+             ]
     end
   end
 
@@ -102,6 +124,25 @@ defmodule PhoneBookWeb.ContactControllerTest do
       conn = get(conn, Routes.contact_path(conn, :show, contact.id))
       assert json_response(conn, 404)["errors"] == ["Forbidden"]
     end
+
+    test "renders errors when request session violates content policy", %{
+      conn: conn,
+      contact: contact
+    } do
+      conn = log_in_user(conn, user_fixture())
+      conn = get(conn, Routes.contact_path(conn, :show, contact.id))
+
+      assert json_response(conn, 404)["errors"] == ["Forbidden"]
+    end
+
+    test "renders errors when data requested doesn't exist", %{conn: conn} do
+      response =
+        assert_error_sent 404, fn ->
+          get(conn, Routes.contact_path(conn, :show, "f53b37df-e281-42f4-a4bd-b7d4c717e19e"))
+        end
+
+      assert {404, [_h | _t], "\"Not Found\""} = response
+    end
   end
 
   describe "update contact" do
@@ -128,6 +169,27 @@ defmodule PhoneBookWeb.ContactControllerTest do
       conn = put(conn, Routes.contact_path(conn, :update, contact), contact: @update_attrs)
       assert json_response(conn, 404)["errors"] == ["Forbidden"]
     end
+
+    test "renders errors when request session violates content policy", %{
+      conn: conn,
+      contact: contact
+    } do
+      conn = log_in_user(conn, user_fixture())
+      conn = put(conn, Routes.contact_path(conn, :update, contact.id), contact: @update_attrs)
+
+      assert json_response(conn, 404)["errors"] == ["Forbidden"]
+    end
+
+    test "renders errors when data requested doesn't exist", %{conn: conn} do
+      response =
+        assert_error_sent 404, fn ->
+          put(conn, Routes.contact_path(conn, :update, "f53b37df-e281-42f4-a4bd-b7d4c717e19e"),
+            contact: @update_attrs
+          )
+        end
+
+      assert {404, [_h | _t], "\"Not Found\""} = response
+    end
   end
 
   describe "delete contact" do
@@ -145,6 +207,16 @@ defmodule PhoneBookWeb.ContactControllerTest do
     test "renders authorization error when data is invalid", %{conn: conn} do
       contact = contact_fixture()
       conn = delete(conn, Routes.contact_path(conn, :delete, contact))
+      assert json_response(conn, 404)["errors"] == ["Forbidden"]
+    end
+
+    test "renders errors when request session violates content policy", %{
+      conn: conn,
+      contact: contact
+    } do
+      conn = log_in_user(conn, user_fixture())
+      conn = delete(conn, Routes.contact_path(conn, :delete, contact), contact: @update_attrs)
+
       assert json_response(conn, 404)["errors"] == ["Forbidden"]
     end
   end
